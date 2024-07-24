788b277e013a595681506fcd5c7e4d8bbfbd6b7c
Béryl HOUESSOU <112707240+beryl-07@users.noreply.github.com>
2024-07-24 21:54:43 +0100

    refactor: Update TaskSchema in data-table
    The commit updates the TaskSchema in the data-table module. The  field now has a minimum length requirement of 1 character, and the  field requires a valid email address. Additionally, default values are set for the , , and  fields.
    
    These changes improve the validation and default behavior of the TaskSchema, ensuring data integrity and enhancing the functionality of the data-table module.

--- task-flow-client/src/pages/data-table/data-table-row-actions.tsx
+++ task-flow-client/src/pages/data-table/data-table-row-actions.tsx
@@ -3,23 +3,28 @@ import { Row } from "@tanstack/react-table";
 import { MoreHorizontal } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import {
-  Dialog, DialogContent,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Icons } from "@/components/icons";
-import {TaskSchema} from "@/pages/data-table/schema.ts";
+import { TaskPriority, TaskSchema, TaskStatus, TaskType } from "@/pages/data-table/schema.ts";
 
 // import { TaskSchema} from "./schema";
-import {DeleteDialog} from "./dialogs/delete-dialog";
+import { DeleteDialog } from "./dialogs/delete-dialog";
 import EditDialog from "@/pages/data-table/dialogs/edit-dialog.tsx";
+import React from "react";
+import { priorities, statuses } from "./filters";
 
 interface DataTableRowActionsProps<TData> {
   row: Row<TData>;
@@ -29,51 +34,133 @@ export function DataTableRowActions<TData>({
   row,
 }: DataTableRowActionsProps<TData>) {
   const [dialogContent, setDialogContent] = useState<React.ReactNode | null>(
-      null
+    null
   );
   const [showDeleteDialog, setShowDeleteDialog] = useState<boolean>(false);
+  const [isUpdatePending, startUpdateTransition] = React.useTransition();
   const task = TaskSchema.parse(row.original);
 
   const handleEditClick = () => {
-    setDialogContent(
-        <EditDialog task={task}/>
-    );
+    setDialogContent(<EditDialog task={task} />);
   };
+  async function editTask(task: TaskType) {
+    return await fetch(`http://127.0.0.1:3333/tasks/${task.id}`, {
+      method: "PATCH",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+  }
 
+  function onSubmit(values: TaskType) {
+    (async () => {
+      const response = await editTask(values);
+      let error = null;
+      if (!response.ok) {
+        const responseBody = await response.json();
+        error = responseBody.error;
+      }
+
+      if (error) {
+        return;
+      }
+      window.location.reload();
+    })();
+  }
   return (
     <Dialog>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant='ghost' className='h-8 w-8 p-0'>
-            <span className='sr-only'>Open menu</span>
-            <MoreHorizontal className='h-4 w-4' />
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent align='end'>
+        <DropdownMenuContent align="end">
           <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuSeparator />
           <DialogTrigger asChild onClick={handleEditClick}>
             <DropdownMenuItem>
-              <Icons.edit className='mr-2 h-4 w-4' />
+              <Icons.edit className="mr-2 h-4 w-4" />
               Edit Details
             </DropdownMenuItem>
           </DialogTrigger>
-
+          <DropdownMenuSeparator />
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>Status</DropdownMenuSubTrigger>
+            <DropdownMenuSubContent>
+              <DropdownMenuRadioGroup
+                value={task.status}
+                onValueChange={(value) => {
+                  startUpdateTransition(() => {
+                    console.log(value);
+                    onSubmit({
+                      ...task,
+                      status:value as TaskStatus
+                    })
+                  });
+                }}
+              >
+                {statuses.map((status, index) => (
+                  <DropdownMenuRadioItem
+                    key={index}
+                    value={status.value}
+                    className="capitalize"
+                    disabled={isUpdatePending}
+                  >
+                    {status.label}
+                  </DropdownMenuRadioItem>
+                ))}
+              </DropdownMenuRadioGroup>
+            </DropdownMenuSubContent>
+          </DropdownMenuSub>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>Priority</DropdownMenuSubTrigger>
+            <DropdownMenuSubContent>
+              <DropdownMenuRadioGroup
+                value={task.priority}
+                onValueChange={(value) => {
+                  startUpdateTransition(() => {
+                    console.log(value);
+                    onSubmit({
+                      ...task,
+                      priority:value as TaskPriority
+                    })
+                  });
+                }}
+              >
+                {priorities.map((priority, index) => (
+                  <DropdownMenuRadioItem
+                    key={index}
+                    value={priority.value}
+                    className="capitalize"
+                    disabled={isUpdatePending}
+                  >
+                    {priority.label}
+                  </DropdownMenuRadioItem>
+                ))}
+              </DropdownMenuRadioGroup>
+            </DropdownMenuSubContent>
+          </DropdownMenuSub>
+          <DropdownMenuSeparator />
           <DropdownMenuItem
             onSelect={() => setShowDeleteDialog(true)}
-            className='text-red-600'
+            className="text-red-600"
           >
-            <Icons.delete className='mr-2 h-4 w-4' />
+            <Icons.delete className="mr-2 h-4 w-4" />
             Delete Details
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
       {dialogContent && <DialogContent>{dialogContent}</DialogContent>}
       <DeleteDialog
-          open={showDeleteDialog}
-          onOpenChange={setShowDeleteDialog}
-          tasks={[task]}
-          showTrigger={false}
-          onSuccess={() => row.toggleSelected(false)}
+        open={showDeleteDialog}
+        onOpenChange={setShowDeleteDialog}
+        tasks={[task]}
+        showTrigger={false}
+        onSuccess={() => row.toggleSelected(false)}
       />
     </Dialog>
   );
--- task-flow-client/src/pages/data-table/schema.ts
+++ task-flow-client/src/pages/data-table/schema.ts
@@ -8,13 +8,13 @@ export type TaskStatus = z.infer<typeof TaskStatus>;
 
 export const TaskSchema = z.object({
   id: z.string().cuid(),
-  title: z.string(),
+  title: z.string().min(1, { message: "Title is Required" }),
   assignedTo:  z
       .string()
-      // .min(1, { message: "Email is Required" })
+      .min(1, { message: "Email is Required" })
       .email({ message: "Must be a valid Email" }),
-  startAt: z.coerce.date(),
-  endAt: z.coerce.date(),
+  startAt: z.coerce.date().default(() => new Date()),
+  endAt: z.coerce.date().default(() => new Date()),
   status: TaskStatus.default("TODO"),
   priority: TaskPriority.default("MEDIUM"),
   createdAt: z.coerce.date().default(() => new Date()),
